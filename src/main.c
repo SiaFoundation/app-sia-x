7908c1c61a90b2687dd9725c72938c50faf454be
Christopher Tarry <chrisjtarry@gmail.com>
2021-05-10 14:07:12 -0400

    add ledger boilerplate bluetooth code

--- src/main.c
+++ src/main.c
@@ -412,8 +412,19 @@ __attribute__((section(".boot"))) int main(void) {
 		BEGIN_TRY {
 			TRY {
 				io_seproxyhal_init();
+
+#if defined(TARGET_NANOX)
+				G_io_app.plane_mode = os_setting_get(OS_SETTING_PLANEMODE, NULL, 0);
+#endif
+
 				USB_power(0);
 				USB_power(1);
+
+#if defined(TARGET_NANOX)
+				BLE_power(0, NULL);
+				BLE_power(1, "Nano X");
+#endif
+
 				ui_idle();
 				sia_main();
 			}
