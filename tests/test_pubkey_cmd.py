bf3c607d5e37ad2df7846278d55ed721e2d2fad0
Christopher Tarry <chrisjtarry@gmail.com>
2023-04-18 14:35:21 -0400

    organize existing tests better and add transaction signing

--- tests/test_pubkey_cmd.py
+++ tests/test_pubkey_cmd.py
@@ -5,7 +5,7 @@ from ragger.backend import RaisePolicy
 from ragger.navigator import NavInsID, NavIns
 from utils import ROOT_SCREENSHOT_PATH
 
-
+# Test will ask to generate a public key that will be accepted on screen
 def test_get_public_key_confirm_accepted(firmware, backend, navigator, test_name):
     client = BoilerplateCommandSender(backend)
     index = 5
@@ -26,41 +26,36 @@ def test_get_public_key_confirm_accepted(firmware, backend, navigator, test_name
                                            test_name,
                                            instructions)
 
-    response = client.get_async_response().data
+    response = client.get_async_response()
     ref_public_key, _ = calculate_public_key_and_chaincode(CurveChoice.Ed25519Slip, path="44'/93'/%d'/0'/0'" % (index))
-    assert(response[:32].hex() == ref_public_key[2:])
+    assert response.status == Errors.SW_OK
+    assert response.data[:32].hex() == ref_public_key[2:]
 
+# Test will ask to generate a public key that will be rejected on screen
 def test_get_public_key_confirm_refused(firmware, backend, navigator, test_name):
     client = BoilerplateCommandSender(backend)
     index = 5
 
-    if firmware.device.startswith("nano"):
-        with client.get_public_key_with_confirmation(index=index):
-            # Disable raising when trying to unpack an error APDU
-            backend.raise_policy = RaisePolicy.RAISE_NOTHING
+    with client.get_public_key_with_confirmation(index=index):
+        # Disable raising when trying to unpack an error APDU
+        backend.raise_policy = RaisePolicy.RAISE_NOTHING
+        if firmware.device.startswith("nano"):
             navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
                                                       [NavInsID.BOTH_CLICK],
                                                       "Reject",
                                                       ROOT_SCREENSHOT_PATH,
                                                       test_name)
-
-        response = client.get_async_response()
-
-        # Assert that we have received a refusal
-        assert response.status == Errors.SW_DENY
-        assert len(response.data) == 0
-    else:
-        with client.get_public_key_with_confirmation(index=index):
-            backend.raise_policy = RaisePolicy.RAISE_NOTHING
+        else:
             navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
                                            test_name,
                                            [NavInsID.USE_CASE_REVIEW_REJECT])
-        response = client.get_async_response()
 
-        # Assert that we have received a refusal
-        assert response.status == Errors.SW_DENY
-        assert len(response.data) == 0
+    response = client.get_async_response()
+    # Assert that we have received a refusal
+    assert response.status == Errors.SW_DENY
+    assert len(response.data) == 0
 
+# Test will ask to generate an address that will be accepted on screen
 def test_get_address_confirm_accepted(firmware, backend, navigator, test_name):
     client = BoilerplateCommandSender(backend)
     index = 5
@@ -81,37 +76,31 @@ def test_get_address_confirm_accepted(firmware, backend, navigator, test_name):
                                            test_name,
                                            instructions)
 
-    response = client.get_async_response().data
+    response = client.get_async_response()
     ref_public_key, _ = calculate_public_key_and_chaincode(CurveChoice.Ed25519Slip, path="44'/93'/%d'/0'/0'" % (index))
-    assert(response.hex()[:64] == ref_public_key[2:])
+    assert response.status == Errors.SW_OK
+    assert response.data[:32].hex() == ref_public_key[2:]
 
+# Test will ask to generate an address that will be rejected on screen
 def test_get_address_confirm_refused(firmware, backend, navigator, test_name):
     client = BoilerplateCommandSender(backend)
     index = 5
 
-    if firmware.device.startswith("nano"):
-        with client.get_address_with_confirmation(index=index):
-            # Disable raising when trying to unpack an error APDU
-            backend.raise_policy = RaisePolicy.RAISE_NOTHING
+    with client.get_address_with_confirmation(index=index):
+        # Disable raising when trying to unpack an error APDU
+        backend.raise_policy = RaisePolicy.RAISE_NOTHING
+        if firmware.device.startswith("nano"):
             navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
                                                       [NavInsID.BOTH_CLICK],
                                                       "Reject",
                                                       ROOT_SCREENSHOT_PATH,
                                                       test_name)
-
-        response = client.get_async_response()
-
-        # Assert that we have received a refusal
-        assert response.status == Errors.SW_DENY
-        assert len(response.data) == 0
-    else:
-        with client.get_public_key_with_confirmation(index=index):
-            backend.raise_policy = RaisePolicy.RAISE_NOTHING
+        else:
             navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
                                            test_name,
                                            [NavInsID.USE_CASE_REVIEW_REJECT])
-        response = client.get_async_response()
 
-        # Assert that we have received a refusal
-        assert response.status == Errors.SW_DENY
-        assert len(response.data) == 0
+    response = client.get_async_response()
+    # Assert that we have received a refusal
+    assert response.status == Errors.SW_DENY
+    assert len(response.data) == 0
