d7ebf92cfe698a208d931a36ce2cdeebd8a3c811
Christopher Tarry <chrisjtarry@gmail.com>
2023-04-18 14:46:20 -0400

    format tests

--- tests/test_name_version.py
+++ tests/test_name_version.py
@@ -1,5 +1,7 @@
 from application_client.boilerplate_command_sender import BoilerplateCommandSender
-from application_client.boilerplate_response_unpacker import unpack_get_app_and_version_response
+from application_client.boilerplate_response_unpacker import (
+    unpack_get_app_and_version_response,
+)
 
 
 # Test a specific APDU asking BOLOS (and not the app) the name and version of the current app
