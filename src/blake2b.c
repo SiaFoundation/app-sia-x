8bdb8a841be8b6456e54c536262a7b26dcdd49a6
Christopher Tarry <chrisjtarry@gmail.com>
2023-11-15 13:14:24 -0500

    replace deprecated functions with new versions

--- src/blake2b.c
+++ src/blake2b.c
@@ -3,17 +3,25 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "sia.h"
+
 void blake2b_init(cx_blake2b_t *S) {
-    cx_blake2b_init(S, 256);
+    if (cx_blake2b_init_no_throw(S, 256) != CX_OK) {
+        THROW(SW_DEVELOPER_ERR);
+    }
 }
 
 void blake2b_update(cx_blake2b_t *S, const uint8_t *in, uint64_t inlen) {
-    cx_hash((cx_hash_t *)S, 0, in, inlen, NULL, 0);
+    if (cx_hash_no_throw((cx_hash_t *)S, 0, in, inlen, NULL, 0) != CX_OK) {
+        THROW(SW_DEVELOPER_ERR);
+    }
 }
 
 void blake2b_final(cx_blake2b_t *S, uint8_t *out, uint64_t outlen) {
     uint8_t buf[32];
-    cx_hash((cx_hash_t *)S, CX_LAST, NULL, 0, buf, sizeof(buf));
+    if (cx_hash_no_throw((cx_hash_t *)S, CX_LAST, NULL, 0, buf, sizeof(buf)) != CX_OK) {
+        THROW(SW_DEVELOPER_ERR);
+    }
     memmove(out, buf, outlen);
 }
 
