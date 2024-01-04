ac19d8c7920119abc519def980a72e7254b8d901
Christopher Tarry <chrisjtarry@gmail.com>
2024-01-04 13:41:56 -0500

    format

--- src/txn.h
+++ src/txn.h
@@ -12,54 +12,56 @@
 #endif
 
 // macros for converting raw bytes to uint64_t
-#define U8BE(buf, off) (((uint64_t)(U4BE(buf, off))     << 32) | ((uint64_t)(U4BE(buf, off + 4)) & 0xFFFFFFFF))
-#define U8LE(buf, off) (((uint64_t)(U4LE(buf, off + 4)) << 32) | ((uint64_t)(U4LE(buf, off))     & 0xFFFFFFFF))
+#define U8BE(buf, off) \
+    (((uint64_t) (U4BE(buf, off)) << 32) | ((uint64_t) (U4BE(buf, off + 4)) & 0xFFFFFFFF))
+#define U8LE(buf, off) \
+    (((uint64_t) (U4LE(buf, off + 4)) << 32) | ((uint64_t) (U4LE(buf, off)) & 0xFFFFFFFF))
 
 // txnDecoderState_e indicates a transaction decoder status
 typedef enum {
-    TXN_STATE_ERR = 1,  // invalid transaction (NOTE: it's illegal to THROW(0))
-    TXN_STATE_PARTIAL,  // no elements have been fully decoded yet
-    TXN_STATE_FINISHED, // reached end of transaction
+    TXN_STATE_ERR = 1,   // invalid transaction (NOTE: it's illegal to THROW(0))
+    TXN_STATE_PARTIAL,   // no elements have been fully decoded yet
+    TXN_STATE_FINISHED,  // reached end of transaction
 } txnDecoderState_e;
 
 // txnElemType_e indicates a transaction element type.
 typedef enum {
-	TXN_ELEM_SC_INPUT,
-	TXN_ELEM_SC_OUTPUT,
-	TXN_ELEM_FC,
-	TXN_ELEM_FCR,
-	TXN_ELEM_SP,
-	TXN_ELEM_SF_INPUT,
-	TXN_ELEM_SF_OUTPUT,
-	TXN_ELEM_MINER_FEE,
-	TXN_ELEM_ARB_DATA,
-	TXN_ELEM_TXN_SIG,
+    TXN_ELEM_SC_INPUT,
+    TXN_ELEM_SC_OUTPUT,
+    TXN_ELEM_FC,
+    TXN_ELEM_FCR,
+    TXN_ELEM_SP,
+    TXN_ELEM_SF_INPUT,
+    TXN_ELEM_SF_OUTPUT,
+    TXN_ELEM_MINER_FEE,
+    TXN_ELEM_ARB_DATA,
+    TXN_ELEM_TXN_SIG,
 } txnElemType_e;
 
 typedef struct {
-    uint8_t elemType; // type of element (txnElemType_e)
+    uint8_t elemType;  // type of element (txnElemType_e)
 
-    uint8_t outVal[1 + 16];    // currency value, Sia-encoded
-    uint8_t outAddr[32];    // address, Sia-encoded
+    uint8_t outVal[1 + 16];  // currency value, Sia-encoded
+    uint8_t outAddr[32];     // address, Sia-encoded
 } txn_elem_t;
 
 // txn_state_t is a helper object for computing the SigHash of a streamed
 // transaction.
 typedef struct {
-	uint8_t buf[510]; // holds raw tx bytes; large enough for two 0xFF reads
-	uint16_t buflen;  // number of valid bytes in buf
-	uint16_t pos;     // mid-decode offset; reset to 0 after each elem
+    uint8_t buf[510];  // holds raw tx bytes; large enough for two 0xFF reads
+    uint16_t buflen;   // number of valid bytes in buf
+    uint16_t pos;      // mid-decode offset; reset to 0 after each elem
 
-	uint16_t elementIndex;
-	txn_elem_t elements[MAX_ELEMS]; // only elements that will be displayed
+    uint16_t elementIndex;
+    txn_elem_t elements[MAX_ELEMS];  // only elements that will be displayed
 
-	uint64_t sliceLen;      // most-recently-seen slice length prefix
-	uint16_t sliceIndex;    // offset within current element slice
+    uint64_t sliceLen;    // most-recently-seen slice length prefix
+    uint16_t sliceIndex;  // offset within current element slice
 
-	uint16_t sigIndex;   // index of TxnSig being computed
-    uint8_t changeAddr[77]; // change address
-	cx_blake2b_t blake;  // hash state
-	uint8_t sigHash[32]; // buffer to hold final hash
+    uint16_t sigIndex;       // index of TxnSig being computed
+    uint8_t changeAddr[77];  // change address
+    cx_blake2b_t blake;      // hash state
+    uint8_t sigHash[32];     // buffer to hold final hash
 } txn_state_t;
 
 // txn_init initializes a transaction decoder, preparing it to calculate the
